64fba20a288d82435854f295397d24fc4616d701
codepainting <mikolaj.dunikowski@modulus.eu>
2022-01-29 12:05:02 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,19 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
+		serviceWorker: {
+			register: true,
+			//			files: (filepath) => !/\.DS_STORE/.test(filepath)
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		target: "#svelte",
 
 		// Override http methods in the Todo forms
 		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+			allowed: ["PATCH", "DELETE"],
+		},
+	},
 };
 
 export default config;
